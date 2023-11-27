17d5966c5e8c85aa5b2a71d8d8ee38f929e98107
Erechiin <erikrodrigues294@gmail.com>
2023-11-27 16:08:42 -0300

    criei aula 11

---
+++ js-curso-em-video/aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`Você está dirigindo à ${vel}km/h`)
+if (vel>60){
+    console.log("Você excedeu a velocidade máxima permitida de 60km/h. MULTADO!")
+}
+console.log("Digija com segurança. Use sempre o cinto de segurança.")
---
+++ js-curso-em-video/aula11/ex09.js
@@ -0,0 +1,18 @@
+var país = "EUA"
+if (país = "Brasil"){
+    console.log("Você é brasileiro")
+} else {
+    console.log("Você é estrangeiro")
+}
+
+//Outra forma de fazer
+
+/*
+
+if (país != Brasil){
+    console.log("Você é estrangeiro")
+} else {
+    console.log("Você é brasileiro")
+}
+
+*/
