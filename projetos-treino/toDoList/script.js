c0178b631b59b8dd4e565fb1481491dfdf36632d
Erechiin <erikrodrigues294@gmail.com>
2025-05-30 20:04:53 -0300

    atualização final
    guardei a lista no local storage para salvar ela e fiz a li ser clicavel e chamar a função itemConluido()

--- projetos-treino/toDoList/script.js
+++ projetos-treino/toDoList/script.js
@@ -17,16 +17,21 @@ const metodos = {
         let createCheck = document.createElement("input")
         createCheck.setAttribute("type","checkbox")
         createCheck.setAttribute("class","selecao")
-        createCheck.setAttribute("data-idDois", index)
+        createCheck.setAttribute("data-id", index)
         return createCheck
     },
-    criarP:(texto)=>{
+    criarP:(texto, index)=>{
         let createP = document.createElement("p")
         createP.innerText = texto
+        createP.classList.add("selecao")
+        createP.setAttribute("data-id", index)
         return createP
     },
     reset:(elemento)=>{
         elemento.innerHTML = ""
+    },
+    atualizarStorage:()=>{
+        localStorage.setItem("lista",JSON.stringify(metodos.listaItens))
     },
     getData:()=>{ //pega o texto mandado pelo usuario e manda pra lista
         const item = document.getElementById("inome")
@@ -38,39 +43,50 @@ const metodos = {
             metodos.listaItens.push({tarefa:itemValue,status:""})
             item.value = ""
             item.setAttribute("placeholder", "Digite algo e tecle enter para enviar")
+            metodos.atualizarStorage()
             metodos.mostrarLista()
         }
     },
     removerItem:(evento)=>{ //remove o item, tanto na ul, quanto na lista
         //verifica se elemento que acionou o evento possui 'btexcluir' como classe
+        const li = evento.target.parentElement; //pega o pai do elemento que acionou o evento
+        const id = evento.target.dataset.id //pega o data-id do elemento que acionou o evento
+        li.remove() //remove a li
+        metodos.listaItens.splice(id,1) //remove o elemento na lista
+        metodos.atualizarStorage()
+        metodos.mostrarLista() 
+    },
+    analiseClick:(evento)=>{
         if(evento.target.classList.contains('btexcluir')){
-            const li = evento.target.parentElement; //pega o pai do elemento que acionou o evento
-            const id = evento.target.dataset.id //pega o data-id do elemento que acionou o evento
-            li.remove() //remove a li
-            metodos.listaItens.splice(id,1) //remove o elemento na lista
-            metodos.mostrarLista() 
+            metodos.removerItem(evento)
+        }else if(evento.target.classList.contains('selecao')){
+            metodos.itemConcluido(evento)
         }
     },
     itemConcluido:(evento)=>{
-        if(evento.target.classList.contains('selecao')){
-            const id = evento.target.dataset.iddois
-            const item = metodos.listaItens[id]
-            const itemStatus = item.status
-            switch(itemStatus){
-                case "concluido":
-                    item.status = ""
-                    break
-                default:
-                    item.status = "concluido"
-                    break
-            }
-            metodos.mostrarLista()
+        const id = evento.target.dataset.id
+        const item = metodos.listaItens[id]
+        const itemStatus = item.status
+        switch(itemStatus){
+            case "concluido":
+                item.status = ""
+                break
+            default:
+                item.status = "concluido"
+                break
         }
+        metodos.atualizarStorage()
+        metodos.mostrarLista()
+        
     },
     mostrarLista:()=>{ //atualiza a lista, os data-id dos botões e mostra os itens na ul
         let listaUl = document.getElementById("lista")
+        const storageDataLista = JSON.parse(localStorage.getItem("lista"))
         if(listaUl.innerHTML != ""){
             metodos.reset(listaUl)
+        }
+        if(storageDataLista!=="" && storageDataLista!==null && storageDataLista!==undefined){
+            metodos.listaItens= [...storageDataLista]
         }
 
         metodos.listaItens.forEach(mostrar)
@@ -80,7 +96,7 @@ const metodos = {
             
             li.appendChild(metodos.criarCheckbox(index))
             
-            li.appendChild(metodos.criarP(itemMostrar.tarefa))
+            li.appendChild(metodos.criarP(itemMostrar.tarefa, index))
             
             li.appendChild(metodos.criarbttn(index)) //manda o index do item na lista como parametro pra função criarbttn(), que define esse index como data-id do botão
             
@@ -107,12 +123,17 @@ const metodos = {
         }
     }
 }
+
+//Mostra a lista quando a pagina carregar (se tiver algo dentro)
+metodos.mostrarLista()
+
 // DELEGAÇÃO DE EVENTOS
 
 const pai = document.querySelector("#lista")
 //addEventListener para quando ouver um click dentro da ul
 pai.addEventListener('click', (event)=>{
-    metodos.removerItem(event)
+    // metodos.removerItem(event)
+    metodos.analiseClick(event)
 })
 pai.addEventListener('change', (event)=>{
     metodos.itemConcluido(event)
